685689fe1394e317ab8ba0d01ae97dbc5ee2c578
Shubhamkumarsaini <activecraft.shubhamsaini@gmail.com>
2026-04-17 10:34:42 +0530

    requirements.txt changes

--- Construction/settings.py
+++ Construction/settings.py
@@ -127,6 +127,9 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
